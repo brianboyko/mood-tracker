d599cb11ad946bf74b104b3fcd123f18a99e5eb7
Brian Boyko <brian.boyko@gmail.com>
2016-05-08 00:02:36 -0500

    Add Readme, structure project

--- client/src/js/store/storeConfig.js
+++ client/src/js/store/storeConfig.js
@@ -3,16 +3,34 @@
 // ==========================
 
 import { createStore, applyMiddleware, compose } from 'redux';
+// Thunk middleware allows us to dispatch functions through our reducers,
+//  instead of just objects.
+// This is useful for writing asynchronous code.
 import thunkMiddleware from 'redux-thunk';
+
+// Multi is a tool that allows us to dispatch multiple actions at once. (It just
+//  helps with keeping code clean and maintainable.);
 import multi from 'redux-multi';
+
+// rootReducer is a collection of all our reducers, put into a single passage.
 import rootReducer from '../reducers';
+
+// Redux Logger will display previous state, the action, and the next state in
+// the console automatically. This is *extremely* helpful for debugging.
 import createLogger from 'redux-logger';
+
+// Eventually, we will have functionality which will allow us to capture all the actions
+// as they are created, and store it in a local variable: localLog. (logToLocal is the
+// function which we use to add items to localLog)
+
 // import { localLog, logToLocal } from '../utilities/loggers'
+
+// We only need
 import { HYDRATE } from '../constants/index'
 
 let logger = createLogger({
   duration: true,
-  timestamp: true,
+  timestamp: true, // I prefer to create timestamps. This way I have an idea of how long it took each action to render; I also can use this data to simulate 'real time' recreations. 
   collapsed: true,
 });
 
@@ -36,20 +54,34 @@ function configureStore(initialState) {
   return storeFunc;
 }
 
+// since both the redux application store and each react component can have a "state,"
+// it can get confusing when referring to them.
+// I'll be referring to the store (here) as "store", or "application store", and the
+// values currently in the storea as the "application state."
+// For components, I'll likely refer to it as the "state" or "component state".
+
 const store = configureStore();
 
+// We may want to take a "snapshot" of the store and save it somewhere, so that we can load
+// an entirely different store.  For example,
+// a progam might be in the middle of an interaction, but the user may wish to
+// pause it to view a tutorial. Instead of coding an entirely different interface
+// for the tutorial, we can take a backup snapshot, hydrate() the store with tutorial
+// data, and when we are done, restore the state we saved.
+//
+// Naturally, we MUST make sure we've got a confirmed backup store before we continue on.
+// this uses a closure so that the backup is stored in memory outside of the function.
+// For semi-obvious reasons, you typically don't want to put your own backup state of the
+// store into the store itself.  Sure, it could be done.  But it seems a bit complex.
 var backup = {};
-
-// we MUST make sure we've got a confirmed backup store before we continue on.
 function backupStore(){
   backup = store.getState();
-  // we must make one quick change to the backup to prevent an infinite loop.
-  // backup.tourActive = true right now (because we just went to tutorial mode.).
-  // however, if we restore THAT, we'll go right back into tutorial mode at the end of tutorial mode.
-  // so we make one tiny change.
-  backup.tourActive = false;
+  // note that we are not *returning* the backup. That's not the point of this function.
+  // we're just making sure that we have a backup in scope for restoreStore().
 }
 
+// Rather than creating a seperate action to restore the store, since "backup" is
+// in scope, let's just dispatch the hydrate action directly.
 function restoreStore(){
   store.dispatch({type: HYDRATE, payload: backup});
 }
--- client/src/js/utilities/reduxify.js
+++ client/src/js/utilities/reduxify.js
@@ -2,26 +2,64 @@
 // ./utilities/reduxify.js
 // desc: Development tools to automatically bind actions and storestate to props.
 // ==========================
+// More Details:
+//
+// Reduxify might not be the "standard" way of doing Redux; certainly,
+// not every component will need access to every action, not everyone will
+// need to pass the dispatcher or the getState() method to their components,
+// and certainly, some users would prefer that each appliction exists on a
+// "need to know basis," and not need access to all values in the store.
+//
+// This is probably best practices for any production app. However, since almost
+// every component that uses Redux has some form of the following code at the
+// end of the file, it made more sense to me to abstract it to a single utility,
+// which you can then import into your container components.  You don't have
+// to remember what to import from 'redux' and 'react-redux'.
+//
+// Additionally, by having this code written only once, it helps keep things
+// DRY.
+//
+// Your mileage may vary.
+// ==========================
+
 
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import {getStore} from '../store/storeConfig'
 
-export function reduxify(actions, component){
+// here we're grabbing "store.getState" from the store, passed in as a function
+// called getStore. We can input that function (or for that matter, any other
+// function,) as a prop to our component.
+
+import { getStore } from '../store/storeConfig'
+
+
+export function reduxify(actions, component){ // pass in all actions you want to bind, and the component itself.
 
   let mapStateToProps = (state) => {
-    // yo dawg...
     state = Object.assign({}, state, {store: state}, {getStore: getStore});
-    // this allows us to access:
-    // -- the entire state with this.props.store
-    // -- an indivdual property "foo" with either this.props.store.foo OR this.props.foo
+    // In the component, we can access:
+    // * this.props[reducer] directly.
+    // * this.props.store[reducer] (there are some who prefer this syntax - see below)
+    // * this.props.getStore() (which allows any function to call store.getState(),
+    //   and pass it in through dependency injection to utilities such as APIs)
+
     return (state);
   }
   let prepareActions = (actions) => (dispatch) =>
     ({ actions: bindActionCreators(actions.default, dispatch),
        dispatch: dispatch,
      })
+     // Here we're taking all of our actions, and binding them to the dispatch,
+     // as normal, but we're also putting it inside an object.
+     // So to access an action, you would have to type
+     // this.props.actions[action name].  This is preferred for a number
+     // of reasons. First, action names and reducers can often be quite
+     // similar, and it can be difficult to tell at a glance if you're
+     // dispatching an action or retrieiving a value from this.props[whatever].
+     // For added clarity, this.props.store can be used, as above, to distinguish
+     // between actions and values in the store.
+
   let mapDispatchToProps = (dispatch) => (prepareActions(actions, dispatch))
 
   return connect(mapStateToProps, mapDispatchToProps)(component);
